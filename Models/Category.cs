24b686b6f1af89d943579f57456c6790621ce0ee
xpnsive03 <marynelle.aban1997@gmail.com>
2024-05-19 01:02:08 +0800

    Add project files.

---
+++ Models/Category.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApplicationForWebSystem.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [StringLength(100)]
+        [DisplayName("Category Name")]
+        public string Name { get; set; }
+		[DisplayName("Display Order")]
+        [Range(1,1000, ErrorMessage ="Display order must be 1-1000!")]
+		public int DisplayOrder { get; set; }
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebApplicationForWebSystem.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
